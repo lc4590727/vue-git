78fdfd4e5a3ddc7b227e5bcc90e142be23a0d5a1
lichao <914940468@qq.com>
2022-06-28 15:04:08 +0800

    master

---
+++ 3.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <list :listData = "data"></list>
+    </div>
+</template>
+
+<script>
+import list from  './2'
+export default {
+    components: list
+}
+</script>
+
+<style>
+
+</style>
